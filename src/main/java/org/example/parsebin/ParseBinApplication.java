1bc35fede1a2feb06ebc35746c2985f61d2fe844
bolatdias <dizzyplay68@gmail.com>
2024-06-17 13:25:54 +0500

    Edit application.properties files

--- src/main/java/org/example/parsebin/ParseBinApplication.java
+++ src/main/java/org/example/parsebin/ParseBinApplication.java
@@ -3,11 +3,13 @@ package org.example.parsebin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableCaching
+@EnableDiscoveryClient
 public class ParseBinApplication {
 
     public static void main(String[] args) {
